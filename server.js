4f0861cf816179ff48cdc05e18bffd7cf9815caa
Парчиев Багаудин Т <student290@ithub.local>
2026-09-21 13:49:14 +0300

    второе испр, сервер с библиотекой express

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const http = require("http")// подключаем модуль http
+const express = require("express")// подключаем библиотеку express
 
+const app = express()// запускаем express
 const PORT = 3000;// порт на котором булем запускать сервер
 
 
-const server = http.createServer((req, res)=>{// сооздаем 
-    res.end("Hello World")// ответ клиенту когда он зайдет на наш сервер
+
+app.get('/' , (req, res)=>{
+    res.send("Главная страница")
 
 })
 
-server.listen(PORT, ()=>{// тут расисываем на каком порту запускается сервер
+
+app.get('/about' , (req, res)=>{
+    res.send("о нас")
+
+})
+
+// const server = http.createServer((req, res)=>{// сооздаем 
+//     res.end("Hello World")// ответ клиенту когда он зайдет на наш сервер
+
+// })
+
+
+app.listen(PORT, ()=>{// тут расисываем на каком порту запускается сервер
     console.log("Сервер запущен, адрес сервера - localhost:3000");// тут делаем уведомление для себя что сервер запущен
     
     })
