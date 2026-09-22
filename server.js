eee294eb5b305fad79a3d943884dd40104c4bc13
Парчиев Багаудин Т <student290@ithub.local>
2026-09-22 10:17:55 +0300

    new

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
-const http = require("http")// подключаем модуль http
-const express = require("express")// подключаем библиотеку express
-
-const app = express()// запускаем express
-const PORT = 3000;// порт на котором булем запускать сервер
+const http = require("http"); // подключаем модуль http
+const express = require("express"); // подключаем библиотеку express
 
 
 
-app.get('/' , (req, res)=>{
-    res.send("Главная страница")
+const app = express(); // запускаем express
+const PORT = 3000; // порт на котором булем запускать сервер
+
+
+app.get("/", (req, res) => {
+  res.send("Главная страница");
+});
+
+
+app.get("/about", (req, res) => {
+  res.send("О нас");
+});
+
+
+app.get("/contact", (req, res) => {
+  res.send("Наши тг, инст, вацап укaзаны здесь -");
+});
+
+
+app.get("/product/:id", (req, res) => {
+  console.log(req);
+
+
+
+  res.send(`Продукты отображаются здесь - ${req.params.id} `);
+});
+
+app.get("/number/:id", (req, res) => {
+  console.log(req);
+
+
+
+  res.send(`Номер отображаются здесь - ${req.params.id} `);
+});
+
+
+app.post(`/addproduct`, (req, res)=>{
+    const{ name , age} = req.body;
+    
 
 })
 
 
-app.get('/about' , (req, res)=>{
-    res.send("о нас")
-
-})
-
-// const server = http.createServer((req, res)=>{// сооздаем 
-//     res.end("Hello World")// ответ клиенту когда он зайдет на наш сервер
-
-// })
-
-
-app.listen(PORT, ()=>{// тут расисываем на каком порту запускается сервер
-    console.log("Сервер запущен, адрес сервера - localhost:3000");// тут делаем уведомление для себя что сервер запущен
-    
-    })
-
-    console.log(123);
-    console.log("sss");
-    
-    
+app.listen(PORT, () => {
+  // тут расписываем на каком порту запускается сервер
+  console.log("Сервер запущен, адрес сервера - localhost:3000"); // тут делаем уведомление для себя что сервер запущен
+});
